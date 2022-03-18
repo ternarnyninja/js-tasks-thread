64b940309894ab3d400333836b0ef057712a56f2
ternarnyninja <markito97jet@gmail.com>
2022-03-19 01:12:38 +0300

    subsscribe-event

--- EventBus.js
+++ EventBus.js
@@ -1,27 +1,43 @@
 class Notifier {
   constructor() {
-    this.callback = [];
+    this.observers = [];
   }
 
   subscribe(callback) {
-    this.callback.push(callback);
+    this.observers.push(callback);
   }
 
   unssubscribe(callback) {
-    this.callback = this.callback.filter(cal => cal !== callback);
+    this.observers = this.observers.filter(sub => sub !== callback);
   }
 
   trigger(payload) {
-    this.callback.forEach(call => {
-        call.change(payload);
-    })
-  }
-}
+    this.observers.forEach(subscribe => subscribe(payload));
+  }}
 
+const blocks = new Notifier();
 
+const block = document.querySelector(".block");
+const block1 = document.querySelector(".block1");
+const block2 = document.querySelector(".block2");
 
+blocks.subscribe(style => {
+  console.log("trigger catched");
+})
 
+const setStyle = style => style ? "orange" : "green";
 
+block.addEventListener("click", () => {
+  blocks.trigger(block.style);
+})
+
+blocks.subscribe(style => {
+  block1.style.backgroundColor = setStyle(style);
+})
+
+blocks.subscribe(style => {
+  block2.style.backgroundColor = setStyle(style);
+})
 
 
 
