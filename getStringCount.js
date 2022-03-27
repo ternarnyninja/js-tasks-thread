206f5c95a4727aa65f3c27a6d22feb49f16282f4
ternarnyninja <markito97jet@gmail.com>
2022-03-28 01:28:31 +0300

    micro-fix

--- getStringCount.js
+++ getStringCount.js
@@ -10,61 +10,58 @@
 // 3. Опционаольно, желательно после 1 и 2:
 // исследовать как хендлит циклические ссылки метод JSON.stringify на mdn
 
-let obj = {
-  first: {
-      v1: "1", 
-      v2: {
-          second: "2"
-      },
-      v3: {
-          third: false,
-          v4: {
-              fourth: ["anytime", 2, 3, 4],
-          }
-
-      },
-      v5: {
-          fifth: null,
-      }
-  }
-}
-
-obj.first.obj = obj;
-
-const getCircularReplacer = () => {
-  const seen = new WeakSet();
-  return (key, value) => {
-    if (typeof value === "object" && value !== null) {
-      if (seen.has(value)) {
-        return;
-      }
-      seen.add(value);
+function getStringCount(object, acc = {result: 0}) {
+  let cache = new WeakMap();
+  const values = Object.values(object);
+  for(let i = 0;i < values.length;i++) {
+    if (typeof values[i] === "string") {
+      acc.result += 1;
+    } else if(typeof values[i] === "object" && values[i] !== null) {
+      getStringCount(values[i], acc);
     }
-    return value;
-  };
-};
-
-const stringJson = JSON.stringify(obj, getCircularReplacer());
-
-const parseJson = () => {
-  return JSON.parse(stringJson);
-}
-
-const objWithOutCircularReference = parseJson(parseJson());
-
-const cache = new WeakMap();
-
-const getStringCount = (obj) => {
-  if (typeof obj === "string") return 1;
-  if (!obj) return 0;
-  if (!cache.has(obj)) {
-    const result = Object.values(obj).reduce((acc, cur) => acc + getStringCount(cur), 0);
-    cache.set(getStringCount, result);
   }
-  return cache.get(getStringCount);
+  return cache;
 }
 
 // debugger;
 
-let res = getStringCount(objWithOutCircularReference);
-console.log(res);
+let obj = {
+  first: "1",
+  second: "2",
+  third: false,
+  fourth: ["anytime", 2, 3, 4],
+  fifth: null,
+};
+
+let result = getStringCount(obj);
+
+console.log(result);
+
+console.log(result.has(obj));
+
+obj = null;
+
+console.log(result.has(obj));
+
+// let obj = {
+//   first: {
+//       v1: "1", 
+//       v2: {
+//           second: "2"
+//       },
+//       v3: {
+//           third: false,
+//           v4: {
+//               fourth: ["anytime", 2, 3, 4],
+//           }
+
+//       },
+//       v5: {
+//           fifth: null,
+//       }
+//   }
+// }
+
+// obj.first.obj = obj;
+
+// let result = getStringCount(obj);
